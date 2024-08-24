4c58b41bf53ea3660cba0717c4a40448f66e4176
June Castillote <june.castillote@gmail.com>
2024-08-24 14:12:46 +0800

    Update Get-ExchangeMailboxSize.ps1

--- Get-ExchangeMailboxSize.ps1
+++ Get-ExchangeMailboxSize.ps1
@@ -6,7 +6,11 @@ param (
     [Parameter()]
     [ValidateSet('KB', 'MB', 'GB', 'TB')]
     [string]
-    $SizeUnitType = 'KB'
+    $SizeUnitType = 'KB',
+
+    [Parameter()]
+    [bool]
+    $FlattenResult = $true
 )
 begin {
     Function ConvertToUnit {
@@ -16,7 +20,7 @@ begin {
             [int] $RoundToDecimal = 2
         )
         $bytes = ([regex]::Match($SizeString, '\((\d{1,3}(,\d{3})*) bytes\)').Groups[1].Value).Replace(',', '')
-        [math]::Round($bytes / ("1$UnitType"), $RoundToDecimal)
+        ([math]::Round($bytes / ("1$UnitType"), $RoundToDecimal) -as [double])
     }
 
     Function GetQuota {
@@ -38,9 +42,12 @@ process {
             Write-Error "The input is not a valid Exchange Mailbox object."
             continue
         }
+        Write-Verbose "$($item.DisplayName)"
 
+        $var_warning = ''
+        $var_note = @("Size values are displayed in ($($SizeUnitType)).")
         try {
-            $mailboxStatistics = Get-MailboxStatistics -Identity $item.Guid.ToString() -WarningVariable warning -WarningAction SilentlyContinue
+            $mailboxStatistics = Invoke-Command { Get-MailboxStatistics -Identity $item.Guid.ToString() } -WarningVariable var_warning
         }
         catch {
             Write-Error "Failed to retrieve mailbox statistics for $($item.DisplayName): $_"
@@ -51,41 +58,60 @@ process {
         $ProhibitSendQuota = GetQuota -quota $item.ProhibitSendQuota -UnitType $SizeUnitType
         $ProhibitSendReceiveQuota = GetQuota -quota $item.ProhibitSendReceiveQuota -UnitType $SizeUnitType
 
-        $TotalItemSize = if ($warning[0] -like "The user hasn't logged on to mailbox*") {
-            0
-        } else {
+        $TotalItemSize = if ($var_warning -like "The user hasn't logged on to mailbox*") {
+            $var_warning = "The TotalItemSize and PercentUsed values cannot be determined because the mailbox has no usage history or is empty."
+            [double]0
+        }
+        else {
             ConvertToUnit -SizeString $mailboxStatistics.TotalItemSize -UnitType $SizeUnitType
-            # ConvertToUnit -SizeString $mailboxStatistics.TotalItemSize.Value -UnitType $SizeUnitType
         }
 
         $PercentUsed = if ($ProhibitSendReceiveQuota -ne 'Unlimited') {
-            [math]::Round(($TotalItemSize / $ProhibitSendReceiveQuota) * 100, 2)
-        } else {
-            0
+            ([math]::Round(($TotalItemSize / $ProhibitSendReceiveQuota) * 100, 5) -as [double])
+        }
+        else {
+            $var_note += 'PercentUsed value is zero (0) because the mailbox has no storage limit.'
+            [double]0
         }
 
         $StorageLimitStatus = switch ($true) {
-            { ($ProhibitSendReceiveQuota -ne 'Unlimited') -and ($TotalItemSize -ge $ProhibitSendReceiveQuota) } { 'Send and Receive Disabled' }
-            { ($ProhibitSendQuota -ne 'Unlimited') -and ($TotalItemSize -ge $ProhibitSendQuota) } { 'Send Disabled' }
-            { ($IssueWarningQuota -ne 'Unlimited') -and ($TotalItemSize -ge $IssueWarningQuota) } { 'Warning' }
-            default { 'Normal' }
+            { ($ProhibitSendReceiveQuota -ne 'Unlimited') -and ($TotalItemSize -ge $ProhibitSendReceiveQuota) } { 'Send and Receive Disabled' ; $var_note += "Mailbox storage is full. It cannot send or receive new items." }
+            { ($ProhibitSendQuota -ne 'Unlimited') -and ($TotalItemSize -ge $ProhibitSendQuota) } { 'Send Disabled' ; $var_note += "Mailbox sending capability is disabled. It can still receive new items." }
+            { ($IssueWarningQuota -ne 'Unlimited') -and ($TotalItemSize -ge $IssueWarningQuota) } { 'Warning' ; $var_note += "Mailbox storage is in warning status. It can still send and receive new items." }
+            default { 'Normal' ; $var_note += "Mailbox size is below any quota." }
         }
 
+        # Combine notes and warning messages
+        $var_note = ($var_note + $var_warning)
+
+        if ($FlattenResult) {
+            # Convert notes from array to a flat numbered list string.
+            # Example:
+            #    [1] Message 1
+            #    [2] Message 2
+            for ($i = 0 ; $i -lt $var_note.Count; $i++) {
+                $var_note[$i] = "[$($i+1)] $($var_note[$i])"
+            }
+            $var_note = $var_note -join "`n"
+        }
+
+        # return result object
         [PSCustomObject]@{
             DisplayName              = $item.DisplayName
-            PrimarySmtpAddress       = $item.PrimarySmtpAddress#.ToString()
+            EmailAddress             = $item.PrimarySmtpAddress.ToString()
+            ProxyAddresses           = $item.EmailAddresses -join ";"
+            MailboxType              = $item.RecipientTypeDetails.ToString()
             IssueWarningQuota        = $IssueWarningQuota
             ProhibitSendQuota        = $ProhibitSendQuota
             ProhibitSendReceiveQuota = $ProhibitSendReceiveQuota
             TotalItemSize            = $TotalItemSize
             PercentUsed              = $PercentUsed
             StorageLimitStatus       = $StorageLimitStatus
-            Notes                    = $warning -join "`n"
+            Notes                    = $var_note
         }
-
-        Write-Verbose "Processed mailbox: $($item.DisplayName)"
     }
 }
 end {
 
 }
+
