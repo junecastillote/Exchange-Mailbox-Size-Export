bd9b3f95742e4a07f3ea967c9ecc0535f0efd5a4
June Castillote <june.castillote@gmail.com>
2024-08-25 15:49:11 +0800

    Create connect-remote-exchange.ps1

---
+++ connect-remote-exchange.ps1
@@ -0,0 +1,3 @@
+# . "C:\Program Files\Microsoft\Exchange Server\V15\Bin\RemoteExchange.ps1"; Connect-ExchangeServer -auto;
+
+. "$($env:ExchangeInstallPath)\Bin\RemoteExchange.ps1"; Connect-ExchangeServer -auto;
