e5a644deaa1f8d123ed3cff4df82a84701e25203
June Castillote <june.castillote@gmail.com>
2024-08-25 15:49:16 +0800

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -4,4 +4,4 @@ $mailbox_collection | .\Get-ExchangeMailboxSize.ps1 -Verbose | Export-Csv .\mail
 
 # Example1 2: Get all mailbox storage quota and status
 $mailbox_collection = Get-Mailbox -ResultSize Unlimited | Sort-Object DisplayName
-$mailbox_collection | .\Get-ExchangeMailboxSize.ps1 -Verbose
+$null = $mailbox_collection | .\Get-ExchangeMailboxSize.ps1 -Verbose -OutVariable result
